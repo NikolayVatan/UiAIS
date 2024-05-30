00b4b2665152555efae1fcb3b1d1f8d88a8f50e9
ageev_n <ageev_n@tochka.com>
2024-05-30 19:26:11 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+
+def guess_the_number():
+    # Генерируем случайное число от 1 до 100
+    number_to_guess = (time.time() * 1000) % 100 + 1
+    attempts = 0
+    guessed = False
+
+    print("Добро пожаловать в игру 'Угадай число'!")
+    print("Я загадал число от 1 до 100. Попробуй угадать его.")
+
+    while not guessed:
+        # Просим пользователя ввести число
+        user_guess = input("Введите ваше предположение: ")
+
+        # Проверяем, что введено число
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+            attempts += 1
+
+            if user_guess < number_to_guess:
+                print("Слишком мало! Попробуйте еще раз.")
+            elif user_guess > number_to_guess:
+                print("Слишком много! Попробуйте еще раз.")
+            else:
+                guessed = True
+                print(f"Поздравляю! Вы угадали число {number_to_guess} за {attempts} попыток.")
+        else:
+            print("Пожалуйста, введите корректное число.")
+
+if __name__ == "__main__":
+    guess_the_number()
